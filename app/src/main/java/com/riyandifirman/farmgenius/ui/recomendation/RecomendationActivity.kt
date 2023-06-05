0370d402dc7564f32c9730e3d6d400212ded86b6
riyandifirman <riyandi.firman08@gmail.com>
2023-06-05 22:28:39 +0700

    configure recomendation page layout

---
+++ app/src/main/java/com/riyandifirman/farmgenius/ui/recomendation/RecomendationActivity.kt
@@ -0,0 +1,12 @@
+package com.riyandifirman.farmgenius.ui.recomendation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riyandifirman.farmgenius.R
+
+class RecomendationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recomendation)
+    }
+}
