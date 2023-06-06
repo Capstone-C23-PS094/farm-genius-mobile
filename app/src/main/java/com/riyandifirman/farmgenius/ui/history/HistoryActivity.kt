e24ffad13203693d512ce66d546ebbcaa3131e7e
riyandifirman <riyandi.firman08@gmail.com>
2023-06-06 19:20:06 +0700

    configure history result detection page layout

--- app/src/main/java/com/riyandifirman/farmgenius/ui/history/HistoryActivity.kt
+++ app/src/main/java/com/riyandifirman/farmgenius/ui/history/HistoryActivity.kt
@@ -3,10 +3,15 @@ package com.riyandifirman.farmgenius.ui.history
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.riyandifirman.farmgenius.R
+import com.riyandifirman.farmgenius.databinding.ActivityHistoryBinding
 
 class HistoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHistoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history)
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/riyandifirman/farmgenius/ui/history/HistoryResultDetectionActivity.kt
@@ -0,0 +1,12 @@
+package com.riyandifirman.farmgenius.ui.history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riyandifirman.farmgenius.R
+
+class HistoryResultDetectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_result_detection)
+    }
+}
