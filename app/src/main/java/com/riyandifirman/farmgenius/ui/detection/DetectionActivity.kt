296ff6ae686c26a7d3c165ca1b508ddef1a7490e
riyandifirman <riyandi.firman08@gmail.com>
2023-06-06 22:42:17 +0700

    configure detection page layout

---
+++ app/src/main/java/com/riyandifirman/farmgenius/ui/detection/DetectionActivity.kt
@@ -0,0 +1,12 @@
+package com.riyandifirman.farmgenius.ui.detection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riyandifirman.farmgenius.R
+
+class DetectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detection)
+    }
+}
