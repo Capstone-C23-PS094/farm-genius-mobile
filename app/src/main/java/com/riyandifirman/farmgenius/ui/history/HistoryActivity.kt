36932157f8aaecfff43a47ed89908b5b459d285c
riyandifirman <riyandi.firman08@gmail.com>
2023-06-06 18:45:33 +0700

    configure history page layout

---
+++ app/src/main/java/com/riyandifirman/farmgenius/ui/history/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.riyandifirman.farmgenius.ui.history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riyandifirman.farmgenius.R
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
