dc59bbe6d39811f7c557d4c82fac7e076a269520
riyandifirman <riyandi.firman08@gmail.com>
2023-06-06 23:00:40 +0700

    configure detection result page layout

--- app/src/main/java/com/riyandifirman/farmgenius/ui/detection/DetectionActivity.kt
+++ app/src/main/java/com/riyandifirman/farmgenius/ui/detection/DetectionActivity.kt
@@ -1,12 +1,24 @@
 package com.riyandifirman.farmgenius.ui.detection
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.riyandifirman.farmgenius.R
+import com.riyandifirman.farmgenius.databinding.ActivityDetectionBinding
 
 class DetectionActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDetectionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detection)
+        binding = ActivityDetectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val detectButton = binding.detectionButton
+        detectButton.setOnClickListener {
+            val intent = Intent(this@DetectionActivity, DetectionResultActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/riyandifirman/farmgenius/ui/detection/DetectionResultActivity.kt
@@ -0,0 +1,12 @@
+package com.riyandifirman.farmgenius.ui.detection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riyandifirman.farmgenius.R
+
+class DetectionResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detection_result)
+    }
+}
