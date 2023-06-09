41e5114c0ffe8aa238968dd731bbb9f0441d06b9
riyandifirman <riyandi.firman08@gmail.com>
2023-06-09 13:44:32 +0700

    configure input parameter in recomendation page

--- app/src/main/java/com/riyandifirman/farmgenius/ui/recomendation/RecomendationActivity.kt
+++ app/src/main/java/com/riyandifirman/farmgenius/ui/recomendation/RecomendationActivity.kt
@@ -3,7 +3,11 @@ package com.riyandifirman.farmgenius.ui.recomendation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatButton
 import com.riyandifirman.farmgenius.R
 import com.riyandifirman.farmgenius.databinding.ActivityRecomendationBinding
 import com.riyandifirman.farmgenius.ui.main.MainActivity
@@ -13,11 +17,68 @@ class RecomendationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRecomendationBinding
     private lateinit var backButton: ImageView
+    private lateinit var temperature: EditText
+    private lateinit var humidity: EditText
+    private lateinit var area: EditText
+    private lateinit var recomendationButton: AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecomendationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Binding
+        temperature = binding.editTextTemperature
+        humidity = binding.editTextHumidity
+        area = binding.editTextHectare
+        recomendationButton = binding.recomendationButton
+
+        setMyButtonEnable()
+
+        // listener untuk kolom suhu
+        temperature.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Do nothing.
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                setMyButtonEnable()
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Do nothing.
+            }
+        })
+
+        // listener untuk kolom kelembapan
+        humidity.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Do nothing.
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                setMyButtonEnable()
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Do nothing.
+            }
+        })
+
+        // listener untuk kolom area
+        area.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Do nothing.
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                setMyButtonEnable()
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Do nothing.
+            }
+        })
 
         // Tombol kembali di klik
         backButton = binding.backButton
@@ -31,5 +92,15 @@ class RecomendationActivity : AppCompatActivity() {
             val intent = Intent(this@RecomendationActivity, RecomendationResultActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    // fungsi untuk mengatur button register
+    private fun setMyButtonEnable() {
+        val temperature = temperature.text
+        val humidity = humidity.text
+        val area = area.text
+        recomendationButton.isEnabled = (temperature != null && temperature.toString()
+            .isNotEmpty()) && (humidity != null && humidity.toString()
+            .isNotEmpty()) && (area != null && area.toString().isNotEmpty())
     }
 }
