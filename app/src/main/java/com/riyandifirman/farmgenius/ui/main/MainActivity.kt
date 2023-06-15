bda8084d182a8b98f6de545a7205ad222b87067c
riyandifirman <riyandi.firman08@gmail.com>
2023-06-15 23:38:05 +0700

    finishing configure history detect section in home page

--- app/src/main/java/com/riyandifirman/farmgenius/ui/main/MainActivity.kt
+++ app/src/main/java/com/riyandifirman/farmgenius/ui/main/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.riyandifirman.farmgenius.R
+import com.riyandifirman.farmgenius.adapter.DetectDiseaseHistoryAdapter
 import com.riyandifirman.farmgenius.databinding.ActivityMainBinding
+import com.riyandifirman.farmgenius.network.ApiConfig
 import com.riyandifirman.farmgenius.network.responses.Data
+import com.riyandifirman.farmgenius.network.responses.GetHistoryResponseItem
 import com.riyandifirman.farmgenius.network.responses.LoginResponse
 import com.riyandifirman.farmgenius.network.responses.RegisterResponse
 import com.riyandifirman.farmgenius.ui.detection.DetectionActivity
@@ -17,6 +23,9 @@ import com.riyandifirman.farmgenius.ui.profile.ProfileActivity
 import com.riyandifirman.farmgenius.ui.recomendation.RecomendationActivity
 import com.riyandifirman.farmgenius.util.Preferences
 import com.riyandifirman.farmgenius.viewmodel.MainViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,6 +47,10 @@ class MainActivity : AppCompatActivity() {
         viewModel.name.observe(this) { name ->
              helloName.text = "Halo, $name!"
         }
+
+        setupRecyclerView()
+
+        Log.d("TOKEN", viewModel.token)
 
         // Set button listener
         val profileButton = binding.rectangleProfile
@@ -63,6 +76,35 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, DetectionActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    // fungsi untuk menampilkan data history penyakit
+    fun setupRecyclerView() {
+        // Mendapatkan data list dari API
+        val token = "Bearer " + viewModel.token
+        val client = ApiConfig.getApiService().getHistoryDisease(token)
+        client.enqueue(object : Callback<List<GetHistoryResponseItem>> {
+            override fun onResponse(
+                call: Call<List<GetHistoryResponseItem>>,
+                response: Response<List<GetHistoryResponseItem>>
+            ) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    val detectDiseaseItems = responseBody as List<GetHistoryResponseItem>
+                    val detectAdapter = DetectDiseaseHistoryAdapter(detectDiseaseItems)
+
+                    binding.rvDeteksi.apply {
+                        layoutManager = LinearLayoutManager(this@MainActivity)
+                        adapter = detectAdapter
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<GetHistoryResponseItem>>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Gagal mendapatkan data", Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
     // Exit app when back button pressed
--- app/src/main/java/com/riyandifirman/farmgenius/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/riyandifirman/farmgenius/viewmodel/MainViewModel.kt
@@ -2,16 +2,24 @@ package com.riyandifirman.farmgenius.viewmodel
 
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.riyandifirman.farmgenius.adapter.DetectDiseaseHistoryAdapter
+import com.riyandifirman.farmgenius.network.ApiConfig
 import com.riyandifirman.farmgenius.network.responses.Data
+import com.riyandifirman.farmgenius.network.responses.GetHistoryResponseItem
 import com.riyandifirman.farmgenius.network.responses.LoginResponse
 import com.riyandifirman.farmgenius.network.responses.RegisterResponse
 import com.riyandifirman.farmgenius.ui.login.LoginActivity
 import com.riyandifirman.farmgenius.ui.main.MainActivity
 import com.riyandifirman.farmgenius.util.Preferences
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel : ViewModel() {
 
@@ -26,11 +34,15 @@ class MainViewModel : ViewModel() {
     private val _id = MutableLiveData<String>()
     val id: LiveData<String> get() = _id
 
+    lateinit var token : String
+
     fun init(context: Context) {
         myPreferences = Preferences(context)
 
         _id.value = myPreferences.getUserId()
         _name.value = myPreferences.getUserName()
         _email.value = myPreferences.getUserEmail()
+        token = myPreferences.getUserToken().toString()
     }
+
 }
