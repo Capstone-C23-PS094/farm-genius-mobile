d634b61485a818c0589177d1720ef8a4886c6165
riyandifirman <riyandi.firman08@gmail.com>
2023-06-16 00:36:45 +0700

    add splash screen

--- app/src/main/java/com/riyandifirman/farmgenius/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/riyandifirman/farmgenius/ui/splash/SplashScreenActivity.kt
@@ -1,12 +1,44 @@
 package com.riyandifirman.farmgenius.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.riyandifirman.farmgenius.R
+import com.riyandifirman.farmgenius.databinding.ActivitySplashScreenBinding
+import com.riyandifirman.farmgenius.ui.login.LoginActivity
+import com.riyandifirman.farmgenius.ui.main.MainActivity
+import com.riyandifirman.farmgenius.util.Preferences
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var myPreferences: Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        myPreferences = Preferences(this)
+
+        // cek status login dari preferences
+        if (myPreferences.getStatusLogin()) {
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        // Animasi
+        val image = binding.splashScreenIcon
+        image.alpha = 0f
+        // Durasi animasi
+        image.animate().setDuration(2000).alpha(1f).withEndAction {
+            // Intent
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            // Animasi
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
